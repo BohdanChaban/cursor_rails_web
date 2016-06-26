7e32aec5abc6ce0010043e3218be00a71474d6c0
Bohdan Chaban <b.chaban.91@gmail.com>
2016-06-26 14:09:18 +0300

    view 2

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,9 @@ module ProductsHelper
     return 'alert-success' if key.to_sym == :create
     'alert-danger' if key.to_sym == :destroy
   end
+
+  def products_list_partial_name(products)
+    return 'empty' if products == 0
+    'list' if products > 0
+  end
 end
