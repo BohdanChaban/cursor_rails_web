e465ea4e6baf7bf5659e700a9933e51dd3d8bd97
Bohdan Chaban <b.chaban.91@gmail.com>
2016-07-08 17:13:36 +0300

    update internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_current_card
   before_action :set_locale
 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-    # I18n.locale = params[:locale] if params[:locale].present?
-  end
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -17,4 +12,10 @@ class ApplicationController < ActionController::Base
     session[:cart_id]||= Cart.create.id
   end
 
+  def set_locale
+    session[:locale] = params[:locale]
+    I18n.locale = session[:locale] if params[:locale].present?
+    # I18n.locale = params[:locale] || I18n.default_locale
+    # I18n.locale = params[:locale] if params[:locale].present?
+  end
 end
