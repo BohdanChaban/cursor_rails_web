056076c1542ae9aed18bbc27a283f61845df77d3
Bohdan Chaban <b.chaban.91@gmail.com>
2016-07-09 22:45:47 +0300

    update rspec testing

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,9 @@
 class OrderMailer < ApplicationMailer
 
   def order_email(email, price)
-    @email = email
     @order_price = price
     @url = 'https://morning-caverns-29306.herokuapp.com'
-    mail(to: @email, subject: 'Order from Cursor Shop')
+    mail(to: email, subject: 'Order from Cursor Shop')
   end
 
 end
--- spec/controllers/cart_controller_spec.rb
+++ spec/controllers/cart_controller_spec.rb
@@ -6,4 +6,13 @@ describe CartsController do
     get :show
     expect(assigns[:carts]).to be_nil
   end
+
+  it 'should send confirmation email' do
+    email = 'example@gmail.com'
+    post :send_mail, email: email
+  end
+
+  it 'should show order'do
+    get :order
+  end
 end
