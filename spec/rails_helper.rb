3811b1fed53601094703873796099d9ab3a0e161
Bohdan Chaban <b.chaban.91@gmail.com>
2016-07-10 17:18:16 +0300

    update Devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,11 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+end
+
+RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
