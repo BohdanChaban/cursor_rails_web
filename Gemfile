97e4a6789ebcf8741984d51e0ab9f91f9fee3944
Bohdan Chaban <b.chaban.91@gmail.com>
2016-07-11 16:23:07 +0300

    add paperclip

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'mailgun_rails'
 gem 'devise'
 gem 'devise-bootstrap-views'
 
+gem 'paperclip', '~> 5.0.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710110939) do
+ActiveRecord::Schema.define(version: 20160711113129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,13 @@ ActiveRecord::Schema.define(version: 20160710110939) do
     t.string   "name"
     t.text     "description"
     t.float    "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "cart_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "products", ["cart_id"], name: "index_products_on_cart_id", using: :btree
